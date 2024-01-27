d2f58ebe8a47143a9a4d5db5184d91a84df2faf4
Ziem0 <ziemekorlikowski@gmail.com>
2024-01-27 09:37:17 +0100

    Initial commit

---
+++ src/main/java/com/example/s27081Bank/S27081BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s27081Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27081BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S27081BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s27081Bank/S27081BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s27081Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S27081BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
