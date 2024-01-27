9e641287afd92cc673c829e99705a0f534ec12c9
Ziem0 <ziemekorlikowski@gmail.com>
2024-01-27 11:11:29 +0100

    Tests

--- src/test/java/com/example/s27081Bank/TransferServiceTest.java
+++ src/test/java/com/example/s27081Bank/TransferServiceTest.java
@@ -60,4 +60,79 @@ public class TransferServiceTest {
         assertThat(transferService.getClient(1).getBalance()).isEqualTo(5000.0);
     }
 
+    @Test
+    void orderTransferTest(){
+        transferService.registerNewClient(1,2500.0);
+
+        transferService.orderTransfer(1,100);
+
+        assertThat(transferStorage.getTransferList().get(0).getClientBalanceStatus()).isEqualTo(2400.0);
+    }
+
+    @Test
+    void notOrderTransferTest(){
+        transferService.registerNewClient(1,2500.0);
+
+        transferService.orderTransfer(1,3000.0);
+
+        assertThat(transferStorage.getTransferList().get(0).getStatus()).isEqualTo(TransferStatus.DECLINED);
+    }
+
+    @Test
+    void addBalanceTest(){
+        transferService.registerNewClient(1,2500.0);
+
+        transferService.addBalance(1,100);
+
+        assertThat(transferStorage.getTransferList().get(0).getClientBalanceStatus()).isEqualTo(2600.0);
+    }
+
+    @Test
+    void notAddBalanceTest(){
+        transferService.registerNewClient(1,2500.0);
+
+        transferService.addBalance(25,100);
+
+        assertThat(transferStorage.getTransferList().get(0).getStatus()).isEqualTo(TransferStatus.DECLINED);
+    }
+
+    @Test
+    void getClientTest(){
+        transferService.registerNewClient(1,2500.0);
+
+        assertThat(transferService.getClient(1)).isNotNull();
+    }
+
+    @Test
+    void notGetClientTest(){
+        transferService.registerNewClient(1,2500.0);
+
+        assertThat(transferService.getClient(10)).isNull();
+    }
+
+    @Test
+    void getClinetBalanceTest(){
+        transferService.registerNewClient(1,2500.0);
+
+        assertThat(transferService.getClientBalance(1)).isEqualTo(2500.0);
+    }
+
+    @Test
+    void notGetClinetBalanceTest(){
+        transferService.registerNewClient(1,2500.0);
+
+        assertThat(transferService.getClientBalance(10)).isEqualTo(0);
+    }
+
+    @Test
+    void doesClientExistTest(){
+        transferService.registerNewClient(1,2500.0);
+        assertThat(transferService.doesClientExist(1)).isEqualTo(true);
+    }
+
+    @Test
+    void notDoesClientExistTest(){
+        transferService.registerNewClient(1,2500.0);
+        assertThat(transferService.doesClientExist(10)).isEqualTo(false);
+    }
 }
